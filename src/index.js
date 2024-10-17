eb5c03a3e7eca6be9003a91e19bbe62c0e35b505
Andy Price <andyprice720@gmail.com>
2024-10-17 21:58:25 +0100

    Style: tasks styling
    Added FontAwesome icons for the edit and delete buttons. Tweaked font
    size of the tasks and the project title and names. Also changed the sidebar
    width and text alinement.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./css/reset.css";
 import "./css/styles.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 import { Projects } from "./projects";
 import { Render } from "./render";
