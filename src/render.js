9311ba2e3e2195ec74dbfd9dddf47a5e46479950
Andy Price <andyprice720@gmail.com>
2024-10-19 21:27:37 +0100

    Style: add task modal
    Styled the add task modal along with the add task button. Added the
    event listeners for opening and closing the modal, as well as the methods
    to open amd close.

--- src/render.js
+++ src/render.js
@@ -5,6 +5,14 @@ export class Render {
 		this.projectsArray = projectsArray;
 
 		this.projectContainer = document.querySelector("[data-project-list]");
+		this.modal = document.querySelector("[data-task-modal]");
+		this.addTaskForm = document.getElementById("add-task");
+
+		this.createTaskBtn = document.querySelector("[data-new-task-btn]");
+		this.cancelTaskBtn = document.querySelector("[data-cancel-task-btn]");
+
+		this.createTaskBtn.addEventListener("click", () => this.showModal());
+		this.cancelTaskBtn.addEventListener("click", () => this.closeModal());
 	}
 
 	renderProject() {
@@ -20,6 +28,15 @@ export class Render {
 
 			this.projectContainer.appendChild(projectElement);
 		});
+	}
+
+	showModal() {
+		this.modal.showModal();
+	}
+
+	closeModal() {
+		this.addTaskForm.reset();
+		this.modal.close();
 	}
 
 	// Better way to remove child as doesn't reparse the whole DOM so more efficient
